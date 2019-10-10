cccc5b3e38f3449d22e01ce216dec4a0562ffde1
Abilkasov Dias <abikasovd@yandex.ru>
2019-10-10 18:09:43 +0600

    Модальное окно + отправка формы

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import showPopup from './modules/showPopup';
 import moreInfo from './modules/moreInfo';
 import tabs from './modules/tabs';
 import cancelTagAEvent from './modules/cancelTagAEvent';
+import addInputForm from './modules/addInputForm';
 import calc from './modules/calc';
 
 //Отмена события у тега a
@@ -24,11 +25,14 @@ showPopup('#call-btn', '.popup-call');
 showPopup('.discount-btn', '.popup-discount');
 showPopup('.check-btn', '.popup-check');
 showPopup('.director-btn', '.popup-consultation');
+showPopup('.call-btn', '.popup-discount');
 //кнопка "больше"
 moreInfo();
 //табы
 tabs('accordion', '.panel-heading-two', '.panel-collapse-two');
 tabs('accordion-two', '.panel-heading', '.panel-collapse');
+
+addInputForm('director-form');
 //калькулятор
 calc();
 
--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,23 +1,70 @@
+import showPopup from "./showPopup";
+
 const calc = () => {
 
     const constructBtn = document.querySelectorAll('.construct-btn'),
         tabcontent = document.querySelectorAll('.panel-collapse-two');
-        console.log('tabcontent: ', constructBtn);
 
         constructBtn.forEach((elem, index) => {
+            let numbersTab = tabcontent.length;
 
             elem.addEventListener('click', () => {
+
                 for(let i = 0; i < tabcontent.length; i++) {
+
+                    tabcontent[i].style.display = 'none';
                     if (index === i) {
                         i++;
                         tabcontent[i].style.display = 'block';
-                    } else {
-                        tabcontent[i].style.display = 'none';
+                    } else if (index === 3) {
+                        numbersTab.style.display = 'block';
                     }
+
                 }
+
             }); 
 
         });
+
+        const checkWell = document.getElementById('myonoffswitch'),
+            secondWell = document.getElementById('second-well'),
+            selectMetr = document.querySelectorAll('.form-control');
+
+        secondWell.style.display = 'none';
+        checkWell.addEventListener('click', () => {
+            let price, result = 0;
+            if (checkWell.checked) {
+                price = 10000;
+                console.log('price: ', price);
+                secondWell.style.display = 'none';
+            } else {
+                price = 15000;
+                console.log('price: ', price);
+                secondWell.style.display = 'block';
+            }
+
+        });
+
+        selectMetr.forEach((elem, index) => {
+            let sel = elem;
+            //.options[elem.selectedIndex].value
+            console.log('sel: ', sel);
+            // if (sel === '1.4 метра') {
+            //     if (sel === '1 штука') {
+            //         result += price;
+            //     } else if (sel === '2 штуки') {
+            //         result += price + price * 0.3;
+            //     } else if (sel === '3 штуки') {
+            //         result += price + price * 0.5;
+            //     }
+            // }
+        });
+
+        // const sum = () => {
+        //     if()
+        // };
+
+        const calcResult = document.getElementById('calc-result');
 
 };
 
